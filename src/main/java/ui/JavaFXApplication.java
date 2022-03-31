3f4e76531c2a812d85fd93a21464564fef040524
Philipp Jenke <philipp.jenke@haw-hamburg.de>
2022-03-31 16:11:11 +0200

    Fixed javafx/lwjgl setup - compile works, runtime not.

---
+++ src/main/java/ui/JavaFXApplication.java
@@ -0,0 +1,21 @@
+package ui;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class JavaFXApplication extends Application {
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    StackPane wurzel = new StackPane();
+    wurzel.getChildren().add(new Button("Ich bin ein Knopf!"));
+    primaryStage.setScene(new Scene(wurzel, 300, 250));
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch();
+  }
+}
